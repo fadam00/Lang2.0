02452a57062e3293ea1429c28a5416ecef9b9571
Frank V. Adames <frankv.adames@outlook.com>
2016-11-08 15:24:02 -0500

    Minor changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery.turbolinks
+//= require bootstrap-sprockets
 //= require_tree .
 //= require turbolinks
-//= require materialize-sprockets
+
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'static_pages#index'
+  
   get '/users/:id', to: 'users#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
